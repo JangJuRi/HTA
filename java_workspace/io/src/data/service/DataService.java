f75ea468eaf9693c0554f67a92697ca3a0a36077
JangJuRi <wkdwnfl119@naver.com>
2020-04-10 17:46:38 +0900

    0410

--- java_workspace/io/src/data/service/DataService.java
+++ java_workspace/io/src/data/service/DataService.java
@@ -1,8 +1,12 @@
 package data.service;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NavigableSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import data.repo.DataRepo;
 import data.vo.Data;
@@ -89,7 +93,40 @@ public class DataService {
 	
 	// 월별 사고현황 통계를 출력하는 기능
 	public void printDataStatByMonth() {
+		List<Data> datas = repo.getAllData();
+		TreeMap<Integer, Stat> stats = new TreeMap<Integer, Stat>();
 		
+		for(Data data : datas) {
+				String day = data.getDay();		// ex) "01-01", "12-23"
+				String monthString = day.substring(0, 2);
+				int month = 0;
+				
+				if(monthString.startsWith("0")) {
+					month = Integer.parseInt(monthString.substring(1));		// ex) "01-01" -> 1
+				} else {
+					month = Integer.parseInt(monthString);					// ex) "12-23" -> 12
+				}
+				
+				Stat saveStat = stats.get(month);			// Map에서 month를 key로 Stat객체를 조회한다.
+				if(saveStat == null) {						// m으로 저장된 Stat객체가 없으면
+					Stat stat = new Stat();					// Stat객체를 생성한다.
+					stat.setDeaths(data.getDeath());		// 사망자수와 사고건수를 담아서
+					stat.setRecords(1);
+					stats.put(month, stat);					// Map에 month로 Stat을 저장한다.
+					
+				} else {									
+					saveStat.setDeaths(saveStat.getDeaths() + data.getDeath());
+					saveStat.setRecords(saveStat.getRecords() + 1);
+				}
+			}
+		
+			System.out.println("월\t사고건수\t사망자수");
+			System.out.println("============================");
+			int size = stats.size();
+			for(int i=1; i<=size; i++) {
+			Stat stat = stats.get(i);
+			System.out.println((i) + "월\t" + stat.getRecords() + "\t" + stat.getDeaths());
+		}
 	}
 	
 	// 요일별 사고현황 통계를 출력하는 기능
@@ -97,17 +134,94 @@ public class DataService {
 		
 	}
 	
-	// 사고현황 요약 결과를 출력하는 기능
-	public void printDataStat() {
+	// 위반내용별 사고현황 통계를 출력하는 기능
+	public void printDataStatByViolation() {
 		
 	}
 	
-	class Stat {
+	// 사고현황 요약 결과를 출력하는 기능
+	public void printDataStat() {
+		// 최다 사고발생 시도, 최다 사고발생 월, 최다 사고발생일, 최다사고발생 요일, 최다사고발생 유형
+		Map<String, Map<String, Stat>> map = new HashMap<String, Map<String,Stat>>();
+		map.put("시도별", new HashMap<String, Stat>());
+		map.put("월별", new HashMap<String, Stat>());
+		map.put("날짜별", new HashMap<String, Stat>());
+		map.put("요일별", new HashMap<String, Stat>());
+		map.put("사고유형별", new HashMap<String, Stat>());
+		
+		Map<String, Stat> sidoMap = map.get("시도별");
+		Map<String, Stat> monthMap = map.get("월별");
+		Map<String, Stat> dayMap = map.get("날짜별");
+		Map<String, Stat> dayOfWeekMap = map.get("요일별");
+		Map<String, Stat> violationMap = map.get("사고유형별");
+		
+		List<Data> datas = repo.getAllData();
+		for(Data data : datas) {
+			String sido = data.getSido();
+			String month = data.getDay().substring(0,2);
+			String day = data.getDay();
+			String dayOfWeek = data.getDayOfWeek();
+			String violation = data.getViolation();
+			int death = data.getDeath();
+			
+			reduceStat(sido, death, sidoMap);
+			reduceStat(month, death, monthMap);
+			reduceStat(day, death, dayMap);
+			reduceStat(dayOfWeek, death, dayOfWeekMap);
+			reduceStat(violation, death, violationMap);
+		}
+		
+		Stat sidoMaxStat = maxState(sidoMap);
+		Stat monthMaxStat = maxState(monthMap);
+		Stat dayMaxStat = maxState(dayMap);
+		Stat dayOfWeekMaxStat = maxState(dayOfWeekMap);
+		Stat violationMaxStat = maxState(violationMap);
+		
+		System.out.println("======= 교통사고 현황 요약 =======");
+		System.out.println("최다 사고 발생 시도 : " + sidoMaxStat.getName() + " " + sidoMaxStat.getRecords() + " " + sidoMaxStat.getDeaths());
+		System.out.println("최다 사고 발생 월 : " + monthMaxStat.getName() + " " + monthMaxStat.getRecords() + " " + monthMaxStat.getDeaths());
+		System.out.println("최다 사고 발생 날짜 : " + dayMaxStat.getName() + " " + dayMaxStat.getRecords() + " " + dayMaxStat.getDeaths());
+		System.out.println("최다 사고 발생 요일 : " + dayOfWeekMaxStat.getName() + " " + dayOfWeekMaxStat.getRecords() + " " + dayOfWeekMaxStat.getDeaths());
+		System.out.println("최다 사고 발생 유형 : " + violationMaxStat.getName() + " " + violationMaxStat.getRecords() + " " + violationMaxStat.getDeaths());
+		
+	}
+	
+	private Stat maxState(Map<String, Stat> map) {
+		Collection<Stat> stats = map.values();
+		TreeSet<Stat> treeSet = new TreeSet<Stat>(stats);
+		return treeSet.last();
+	}
+	
+	
+	private void reduceStat(String key, int death, Map<String, Stat> map) {
+		if(map.containsKey(key)) {
+			Stat stat = map.get(key);
+			stat.setRecords(stat.getRecords() + 1);
+			stat.setDeaths(stat.getDeaths() + death);
+		} else {
+			Stat stat = new Stat();
+			stat.setName(key);
+			stat.setRecords(1);
+			stat.setDeaths(death);
+			map.put(key, stat);
+		}
+	}
+	
+	class Stat implements Comparable<Stat>{
+		private String name;
 		private int records;
 		private int deaths;
 		
 		public Stat() {}
 
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
 		public int getRecords() {
 			return records;
 		}
@@ -124,5 +238,11 @@ public class DataService {
 			this.deaths = deaths;
 		}
 		
+		@Override
+		public int compareTo(Stat o) {
+			return this.deaths - o.deaths;
+		}
 	}
+
+	
 }
