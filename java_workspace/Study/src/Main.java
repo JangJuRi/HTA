8db3fb1377a2ca95c2aaf45b02e0f09585d8ce99
JangJuRi <wkdwnfl119@naver.com>
2020-05-15 17:50:42 +0900

    0515

--- java_workspace/Study/src/Main.java
+++ java_workspace/Study/src/Main.java
@@ -1,31 +1,7 @@
-import java.util.Scanner;
 
 public class Main {
    
    public static void main(String[] args) {
-      
-      int first = 0;
-      int second = 0;
-      int temp = 0;
-      int count = 0;
-      
-      Scanner sc = new Scanner(System.in);
-      int number = sc.nextInt();
-      
-      first = number / 10;   
-      second = number % 10;   
-      
-      while(true) {
-         
-         count++;
-         temp = second;
-         second = (first + second) % 10;
-         first = temp;
-         
-         if(number == first*10 + second) {
-            break;
-         }
-      }
-      System.out.println(count);
+	   
    }
 }
